95c49e960d9b517dd51369296a55e3557d81ed51
mrk.gvrr <marcangelguevarra501@gmail.com>
2026-05-16 03:44:59 +0800

    Update verify-mlbb.js

--- api/verify-mlbb.js
+++ api/verify-mlbb.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function (req, res) {
  res.setHeader('Access-Control-Allow-Origin', 'https://mark-shop-beryl.vercel.app');
   const { id, zone } = req.query;
 
